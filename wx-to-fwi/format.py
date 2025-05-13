5ad683046dde417cad26ff67103a0d20f2285671
bobby-payne <gpayne1654@uvic.ca>
2025-05-13 14:47:17 -0700

    feat/functions for renaming data and stacking spatial dims

---
+++ wx-to-fwi/format.py
@@ -0,0 +1,113 @@
+import xarray as xr
+
+
+def rename_wx_variables(data: xr.Dataset) -> xr.Dataset:
+    """
+    Renames weather variables in the dataset to an format common to all
+    functions in the wx-to-fwi package. Specifically, the temperature
+    variable should be named 'TEMP', relative humidity should be 'RH',
+    wind speed should be 'WS', and precipitation should be 'PREC'.
+
+    Parameters
+    ----------
+    data : xr.Dataset
+        The dataset containing the weather variables.
+
+    Returns
+    -------
+    xr.Dataset
+        The dataset with renamed variables.
+    """
+
+    # Common alternative names for certain weather variables
+    alternative_TEMP_names = ['T2', 'Temperature', 'T', 'Temp', 'temp', 'temperature']
+    alternative_RH_names = ['RH2', 'rh', 'RelativeHumidity', 'relativehumidity']
+    alternative_WS_names = ['WS10', 'WindSpeed', 'WindSpeed10m']
+    alternative_PREC_names = ['Precipitation', 'Precip', 'Rainfall', 'Rain', 'prec', 'precip']
+
+    # Loop through variables in the dataset and rename them if they match any of the alternative names
+    for var in data.data_vars:
+
+        if var in alternative_TEMP_names:
+            data = data.rename({var: 'TEMP'})
+        elif var in alternative_RH_names:
+            data = data.rename({var: 'RH'})
+        elif var in alternative_WS_names:
+            data = data.rename({var: 'WS'})
+        elif var in alternative_PREC_names:
+            data = data.rename({var: 'PREC'})
+
+    return data
+
+
+def rename_coordinates(data: xr.Dataset) -> xr.Dataset:
+    """
+    Renames coordinate variables in the dataset to a format common to all
+    functions in the wx-to-fwi package. Specifically, the latitude and
+    longitude coordinates should be named 'lat' and 'long'.
+
+    Parameters
+    ----------
+    data : xr.Dataset
+        The dataset containing the coordinate variables.
+
+    Returns
+    -------
+    xr.Dataset
+        The dataset with renamed coordinate variables.
+    """
+
+    # Common alternative names for latitude and longitude coordinates
+    alternative_latitude_names = ['lat', 'latitude', 'LAT', 'LATITUDE', 'XLAT']
+    alternative_longitude_names = ['lon', 'longitude', 'LON', 'LONGITUDE', 'XLONG', 'LONG']
+    alternative_time_names = ['TIME', 'Time']
+
+    # Loop through coordinates in the dataset and rename them if they match any of the alternative names
+    for coord in data.coords:
+
+        if coord in alternative_latitude_names:
+            data = data.rename({coord: 'lat'})
+        elif coord in alternative_longitude_names:
+            data = data.rename({coord: 'long'})
+        elif coord in alternative_time_names:
+            data = data.rename({coord: 'time'})
+
+    return data
+
+
+def stack_spatial_dims(data: xr.Dataset, x_name: str = 'rlon', y_name: str = 'rlat') -> xr.Dataset:
+    """
+    Stacks the spatial dimensions (given by x_name and y_name) of the dataset into a single
+    coordinate variable.
+
+    Parameters
+    ----------
+    data : xr.Dataset
+        The dataset containing the spatial coordinates.
+
+    Returns
+    -------
+    xr.Dataset
+        The dataset with stacked spatial coordinates.
+    """
+
+    # Stack the latitude and longitude coordinates into a single coordinate variable
+    data = data.stack(xy=[x_name, y_name])
+
+    return data
+
+
+
+
+
+# # TESTING; TEMPORARY
+# from glob import glob
+# years = [2008]
+# paths = []
+# for year in years:
+#     paths += sorted(glob(f"/users/rpayne/data/unproc/WRF/ctl/TAS/*.nc"))
+# data = xr.open_mfdataset(paths).isel(rlat=slice(20,276),rlon=slice(110,366)).sel(time=slice(f"{years[0]}-01-01",f"{years[-1]}-12-31"))
+# data = rename_wx_variables(data)
+# data = rename_coordinates(data)
+# data = stack_spatial_dims(data)
+# print(data.dims)
