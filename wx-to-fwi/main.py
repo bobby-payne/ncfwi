083bfa66b81e7fda5e6964bbef698975246bccf1
bobby-payne <gpayne1654@uvic.ca>
2025-06-11 10:19:51 -0700

    typo + remove unused numba library

--- wx-to-fwi/main.py
+++ wx-to-fwi/main.py
@@ -1,13 +1,11 @@
 import numpy as np
 import xarray as xr
 import pandas as pd
-import numba
 import time
 import gc
 from utils import *
 from inout import *
 from season import *
-numba.set_num_threads(4)
 
 # Load in the data and do a few preprocessing steps
 print("Loading data...")
@@ -18,17 +16,17 @@ data = transpose_dims(data)
 data = rename_coordinates(data)
 data = rename_wx_variables(data)
 data = apply_transformations(data)
-# data = apply_spatial_indexing(data)
+data = apply_spatial_crop(data)
 
 # Obtain the fire season mask
 print("Calculating maximum daily temperature...")
 daily_max_temp_data = get_max_daily_temperature(data)
 
-# print("Computing fire season... (this will take a few minutes)")
-# start_time = time.time()
-# fire_season_mask = compute_fire_season(daily_max_temp_data, return_as_xarray=True)
-# end_time = time.time()
-# print(f"compute_fire_season took {end_time - start_time:.2f} seconds")
-# print(fire_season_mask.shape)
+print("Computing fire season... (this will take a few minutes)")
+start_time = time.time()
+fire_season_mask = compute_fire_season(daily_max_temp_data, return_as_xarray=True)
+end_time = time.time()
+print(f"compute_fire_season took {end_time - start_time:.2f} seconds")
+print(fire_season_mask.shape)
 
 gc.collect()
