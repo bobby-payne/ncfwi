a94b1431c3039788d768fbe776dae0fd8a3a4234
bobby-payne <gpayne1654@uvic.ca>
2025-06-18 16:00:33 -0700

    function for reducing file opening time via open_mfdataset's preproc

--- ncfwi/readwrite.py
+++ ncfwi/readwrite.py
@@ -1,5 +1,8 @@
 import xarray as xr
+import pandas as pd
 import os
+from typing import Callable
+from types import NoneType
 
 from config import get_config
 
@@ -22,10 +25,49 @@ def get_paths_to_wx_data() -> dict:
     return path_dictionary
 
 
+def restrict_timespan_before_merging(wx_data: xr.Dataset) -> xr.Dataset | NoneType:
+    """
+    A preprocessing function for xarray.open_mfdataset to only select data
+    for the time range specified in the configuration file. This does two things:
+    (a) ensures that the time dim for each wx variable is the same so that they
+    can be merged, and (b) speeds up open_mfdataset. Only meant to be used as an 
+    argument to `xarray.open_mfdataset`'s `preprocess` parameter.
+
+    Parameters
+    ----------
+    wx_data : xarray.Dataset
+        An xarray dataset with a time dimension as specified in the config.
+
+    Returns
+    -------
+    xarray.Dataset | NoneType
+        The dataset filtered to only include data from the specified year.
+        If no data is found for the specified time range, returns None.
+    """
+
+    config = get_config()
+    time_dim_name = config["data_vars"]["t_dim_name"]
+    start_year = config["settings"]["start_year"]
+    end_year = config["settings"]["end_year"]
+    data_timerange = pd.to_datetime(wx_data[time_dim_name].values)
+    if not any((start_year <= t.year <= end_year) for t in data_timerange):
+        return wx_data.isel({time_dim_name: slice(0, 0)})
+    else:
+        return wx_data.sel({
+            time_dim_name: slice(f"{start_year}-01-01", f"{end_year}-12-31")
+            })
+
+
 def load_wx_data() -> xr.Dataset:
     """
     Open each wx data (assumed netcdf) separately and then
     merge them into a single xarray dataset.
+
+    Parameters
+    ----------
+    preprocessing_function : Callable, optional
+        A function passed on to `xarray.open_mfdataset` on how to
+        preprocess the datasets in the data directory before merging.
 
     Returns
     -------
@@ -43,7 +85,12 @@ def load_wx_data() -> xr.Dataset:
     for wx_var in wx_var_names:
         print("Opening data:", wx_var)
         path = wx_data_paths[wx_var]
-        wx_data[wx_var] = xr.open_mfdataset(path)
+        wx_data[wx_var] = xr.open_mfdataset(
+            path,
+            preprocess=restrict_timespan_before_merging,
+            combine="nested",
+            concat_dim=get_config()["data_vars"]["t_dim_name"],
+            )
 
     # Merge into an xarray.Dataset
     wx_data_xarray = xr.merge(wx_data.values(), join="exact")
