07c87e1295d3e8748a6211b940c66c7de557c423
bobby-payne <gpayne1654@uvic.ca>
2025-07-04 10:31:24 -0700

    function for overwintering drought code

--- ncfwi/season.py
+++ ncfwi/season.py
@@ -219,3 +219,38 @@ def get_winter_precip_accum_for_grid_point(
         winter_precip_accum = np.sum(hourly_precipitation_array[transition_indices[-1] + 1:])
 
     return winter_precip_accum
+
+
+def overwinter_DC (final_fall_DC: float,
+                   winter_precip_accum: float,
+                   ) -> float:
+    """
+    Overwinter the Drought Code (DC) using the accumulated precipitation
+    after the fire season ends.
+
+    Parameters
+    ----------
+    final_fall_DC : float
+        The Drought Code value at the end of the previous fire season.
+    winter_precip_accum : float
+        The accumulated precipitation after the previous fire season ends.
+
+    Returns
+    -------
+    float
+        The new Drought Code value after overwintering.
+    """
+    
+    config = get_config()
+    carry_over_fraction = config["calculation_parameters"]["carry_over_fraction"]
+    wetting_efficiency_fraction = config["calculation_parameters"]["wetting_efficiency_fraction"]
+
+    # Overwintering logic for Drought Code
+    fall_moisture_equivalent_DC = 800. * np.exp(-final_fall_DC / 400.)
+    spring_moisture_equivalent_DC = (
+        carry_over_fraction * fall_moisture_equivalent_DC
+        + 3.94 * winter_precip_accum * wetting_efficiency_fraction
+    )
+    startup_DC = 400. * np.log(spring_moisture_equivalent_DC / 800.)
+
+    return startup_DC
